1694ad20b3492cf9a5107fbb2e2049b22f42c237
imane-cloud <72994746+imane-cloud@users.noreply.github.com>
2021-04-01 18:34:01 +0100

    Update plot_of_altcoins.py

--- plot_of_altcoins.py
+++ plot_of_altcoins.py
@@ -1,35 +1,29 @@
-import time
-import requests
+from datetime import datetime
+import pickle
 import pandas as pd
-import json
-from bs4 import BeautifulSoup
+def get_json_data(json_url, cache_path):
+    '''Download and cache JSON data, return as a dataframe.'''
+    try:        
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(json_url))
+    except (OSError, IOError) as e:
+        print('Downloading {}'.format(json_url))
+        df = pd.read_json(json_url)
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(json_url, cache_path))
+    return df
+base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'
+start_date = datetime.strptime('2015-01-01', '%Y-%m-%d') # get data from the start of 2015
+end_date = datetime.now() # up until today
+pediod = 86400 # pull daily data (86,400 seconds per day)
 
-cmc = requests.get("https://coinmarketcap.com/currencies/")
-soup = BeautifulSoup(cmc.content, 'html.parser')
-# print(soup.prettify())
-data = soup.find('script', id="__NEXT_DATA__", type="application/json")
-coins = {}
-coin_data = json.loads(data.contents[0])
-listings = coin_data['props']['initialState']['cryptocurrency']['listingLatest']['data']
-for i in listings:
-    coins[str(i['id'])] = i['slug']
-
-
-def get_timestamp(datetime: str):
-    return int(time.mktime(time.strptime(datetime, '%Y-%m-%d %H:%M:%S')))
-
-
-for i in coins:
-    data = get_currencies_quotes(start_date='2021-03-20 00:00:00', end_date='2021-03-26 00:00:00', id_currency=i)
-    data_flat = [quote['quote']['USD'] for quote in data['data']['quotes']]
-    df = pd.DataFrame(data_flat)
-    #print(coins[i])
-    df.set_index('timestamp', inplace=True)
-
-    pd.set_option('display.max_rows', None)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.width', 1000)
-    pd.set_option('display.colheader_justify', 'center')
+def get_crypto_data(poloniex_pair):
+    '''Retrieve cryptocurrency data from poloniex'''
+    json_url = base_polo_url.format(poloniex_pair, start_date.timestamp(), end_date.timestamp(), pediod)
+    data_df = get_json_data(json_url, poloniex_pair)
+    data_df = data_df.set_index('date')
+    return data_df
 altcoins = ['ETH','LTC','XRP','ETC','STR','DASH','SC','XMR','XEM']
 
 altcoin_data = {}
@@ -40,9 +34,10 @@ for altcoin in altcoins:
 altcoin_data['ETH'].tail()
 df_eth = altcoin_data['ETH']
 df_ltc = altcoin_data['LTC']
-df_etc = altcoin_data['ETC']
 df = pd.DataFrame({'ETH': df_eth.close,
-                   'LTC': df_ltc.close,
-                  'ETC':df_str.close})
+                   'LTC': df_ltc.close})
 df.head()
 df.plot(grid=True, figsize=(15, 10))
+
+
+
