fc423a02314d36eea82c61bab17e62ba2ac5d45f
imane-cloud <72994746+imane-cloud@users.noreply.github.com>
2021-04-16 11:15:15 +0000

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,3 +1,10 @@
+#Mesurer la corrélation des cours de cloture
+'''Nous calculons la corrélation de Pearson entre les cours de clôture de BCH, ETH, LTC, BTC, XMR, ETC et USD. La corrélation de Pearson est une mesure de la corrélation linéaire
+entre deux variables X et Y. Elle a une valeur comprise entre +1 et −1, où 1 est la corrélation linéaire positive totale, 0 est aucune corrélation linéaire et −1 est la linéaire négative totale corrélation. 
+La matrice de corrélation est symétrique, nous ne montrons donc que la moitié inférieure.
+Sifr Data met à jour quotidiennement les corrélations Pearson pour de nombreuses crypto-monnaies.'''
+
+
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
