a8f04d3551deee0e6e92d12922a413c56eec918a
imane-cloud <72994746+imane-cloud@users.noreply.github.com>
2021-04-18 00:27:13 +0000

    Update cnvert_to_usd.py

--- cnvert_to_usd.py
+++ cnvert_to_usd.py
@@ -1,30 +1,28 @@
-from datetime import datetime
+from datetime import datetime #permet la manipulation des dates
 import pickle
 import pandas as pd
 import plotly.offline as py
 import plotly.express as px
 def get_json_data(json_url, cache_path):
-    '''Download and cache JSON data, return as a dataframe.'''
-    try:        
-        f = open(cache_path, 'rb')
-        df = pickle.load(f)   
-        print('Loaded {} from cache'.format(json_url))
-    except (OSError, IOError) as e:
-        print('Downloading {}'.format(json_url))
-        df = pd.read_json(json_url)
-        df.to_pickle(cache_path)
-        print('Cached {} at {}'.format(json_url, cache_path))
-    return df
-base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'
+    '''Téléchargez et mettez en cache les données JSON, retournez-les sous forme de dataframe.'''
+    '''JSON est un raccourci pour la notation d'objets JavaScript. Il s'agit d'un format de texte 
+            souvent utilisé pour échanger des données sur le Web.'''
+    df = pd.read_json(json_url)#Convertissez une chaîne JSON en un Pandas DataFrame.
+    df.to_pickle(cache_path)# Pickle (sérialiser) l'objet dans le fichier.
+    print('Cached {} at {}'.format(json_url, cache_path))# c est la phrase qui s'affiche avant la visualisation du graphe
+    return df#df c est la data frame
+base_polo_url = 'https://poloniex.com/public?command=returnChartData&currencyPair={}&start={}&end={}&period={}'#le site dont on a prit la data
 start_date = datetime.strptime('2015-01-01', '%Y-%m-%d') # get data from the start of 2015
-end_date = datetime.now() # up until today
-pediod = 86400 # pull daily data (86,400 seconds per day)
+end_date = datetime.now() # jusqu'à aujourd'hui
+pediod = 86400 # extraire des données quotidiennes (86 400 secondes par jour)
 
 def get_crypto_data(poloniex_pair):
-    '''Retrieve cryptocurrency data from poloniex'''
+    '''Récupérer les données de crypto-monnaie de poloniex'''
     json_url = base_polo_url.format(poloniex_pair, start_date.timestamp(), end_date.timestamp(), pediod)
+    #il remplace la premiére accolade par poloniex_pair, le deuxieme par le nombre de seconde depuis start_date,la troisieme par le nombre de seconde depuis end_date,et a la fin par 86400
+    #La fonction timestamp () renvoie l'heure exprimée en nombre de secondes
     data_df = get_json_data(json_url, poloniex_pair)
-    data_df = data_df.set_index('date')
+    data_df = data_df.set_index('date')#Définir l'index pour qu'il devienne la colonne "date"
     return data_df
 altcoins = ['USDC_ETH','USDC_LTC','USDC_BTC','USDC_ETC','USDC_STR','USDC_BCH','USDC_XMR']
 
@@ -46,8 +44,7 @@ df = pd.DataFrame({'ETH': df_eth.close,
                   'bch': df_bch.close,
                   'xmr':df_xmr.close
                   })
-#df.head()
-#df.plot(grid=True, figsize=(15, 10))
+#plot des altcoins
 df=df.fillna(0)
 fig = px.line(df)
 py.plot(fig)
